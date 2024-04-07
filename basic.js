6c2594c0293b3433a8d6467bbda6175fc290414c
Chinexcee <148772525+Chinexcee@users.noreply.github.com>
2024-04-08 00:33:31 +0100

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,21 @@
+// Function to calculate the area of a rectangle
+function calculateArea(length, width) {
+  return (Area)= length * width;
+}
+
+// Function to reverse a string
+function reverseString(str) {
+  return str.split("").reverse().join("");
+
+}
+
+// Function to check if a number is even
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+// Test cases
+console.log(calculateArea(6, 4)); // Output: 24
+console.log(reverseString("Chinedu")); // Output: "udenihC"
+console.log(isEven(8)); // Output: true
+console.log(isEven(7)); // Output: false
